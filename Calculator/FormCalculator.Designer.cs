a87263a87ad7965157043c240a2976e99a50d298
Jona Hammann <info.minejona@gmail.com>
2022-09-11 19:31:42 +0200

    Add files via upload

---
+++ Calculator/FormCalculator.Designer.cs
@@ -0,0 +1,332 @@
+﻿
+namespace Taschenrechner
+{
+  partial class Tachenrechner
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tachenrechner));
+      this.CmdNumber1 = new System.Windows.Forms.Button();
+      this.CmdNumber2 = new System.Windows.Forms.Button();
+      this.CmdNumber3 = new System.Windows.Forms.Button();
+      this.CmdNumber6 = new System.Windows.Forms.Button();
+      this.CmdNumber5 = new System.Windows.Forms.Button();
+      this.CmdNumber4 = new System.Windows.Forms.Button();
+      this.CmdNumber7 = new System.Windows.Forms.Button();
+      this.CmdNumber8 = new System.Windows.Forms.Button();
+      this.CmdNumber9 = new System.Windows.Forms.Button();
+      this.CmdNumber0 = new System.Windows.Forms.Button();
+      this.CmdOperationResult = new System.Windows.Forms.Button();
+      this.CmdOperationPlus = new System.Windows.Forms.Button();
+      this.CmdOperationMinus = new System.Windows.Forms.Button();
+      this.CmdOperationMultiply = new System.Windows.Forms.Button();
+      this.CmdOperationComma = new System.Windows.Forms.Button();
+      this.CmdOperationRoot = new System.Windows.Forms.Button();
+      this.LblResultShow = new System.Windows.Forms.Label();
+      this.CmdReset = new System.Windows.Forms.Button();
+      this.CmdOperationDivide = new System.Windows.Forms.Button();
+      this.LblCopyright = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // CmdNumber1
+      // 
+      this.CmdNumber1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber1.Location = new System.Drawing.Point(22, 351);
+      this.CmdNumber1.Name = "CmdNumber1";
+      this.CmdNumber1.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber1.TabIndex = 0;
+      this.CmdNumber1.Text = "1";
+      this.CmdNumber1.UseVisualStyleBackColor = true;
+      this.CmdNumber1.Click += new System.EventHandler(this.CmdNumber1_Click);
+      // 
+      // CmdNumber2
+      // 
+      this.CmdNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber2.Location = new System.Drawing.Point(92, 351);
+      this.CmdNumber2.Name = "CmdNumber2";
+      this.CmdNumber2.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber2.TabIndex = 1;
+      this.CmdNumber2.Text = "2";
+      this.CmdNumber2.UseVisualStyleBackColor = true;
+      this.CmdNumber2.Click += new System.EventHandler(this.CmdNumber2_Click);
+      // 
+      // CmdNumber3
+      // 
+      this.CmdNumber3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber3.Location = new System.Drawing.Point(162, 351);
+      this.CmdNumber3.Name = "CmdNumber3";
+      this.CmdNumber3.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber3.TabIndex = 2;
+      this.CmdNumber3.Text = "3";
+      this.CmdNumber3.UseVisualStyleBackColor = true;
+      this.CmdNumber3.Click += new System.EventHandler(this.CmdNumber3_Click);
+      // 
+      // CmdNumber6
+      // 
+      this.CmdNumber6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber6.Location = new System.Drawing.Point(162, 274);
+      this.CmdNumber6.Name = "CmdNumber6";
+      this.CmdNumber6.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber6.TabIndex = 3;
+      this.CmdNumber6.Text = "6";
+      this.CmdNumber6.UseVisualStyleBackColor = true;
+      this.CmdNumber6.Click += new System.EventHandler(this.CmdNumber6_Click);
+      // 
+      // CmdNumber5
+      // 
+      this.CmdNumber5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber5.Location = new System.Drawing.Point(92, 274);
+      this.CmdNumber5.Name = "CmdNumber5";
+      this.CmdNumber5.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber5.TabIndex = 4;
+      this.CmdNumber5.Text = "5";
+      this.CmdNumber5.UseVisualStyleBackColor = true;
+      this.CmdNumber5.Click += new System.EventHandler(this.CmdNumber5_Click);
+      // 
+      // CmdNumber4
+      // 
+      this.CmdNumber4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber4.Location = new System.Drawing.Point(22, 274);
+      this.CmdNumber4.Name = "CmdNumber4";
+      this.CmdNumber4.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber4.TabIndex = 5;
+      this.CmdNumber4.Text = "4";
+      this.CmdNumber4.UseVisualStyleBackColor = true;
+      this.CmdNumber4.Click += new System.EventHandler(this.CmdNumber4_Click);
+      // 
+      // CmdNumber7
+      // 
+      this.CmdNumber7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber7.Location = new System.Drawing.Point(22, 196);
+      this.CmdNumber7.Name = "CmdNumber7";
+      this.CmdNumber7.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber7.TabIndex = 6;
+      this.CmdNumber7.Text = "7";
+      this.CmdNumber7.UseVisualStyleBackColor = true;
+      this.CmdNumber7.Click += new System.EventHandler(this.CmdNumber7_Click);
+      // 
+      // CmdNumber8
+      // 
+      this.CmdNumber8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber8.Location = new System.Drawing.Point(92, 196);
+      this.CmdNumber8.Name = "CmdNumber8";
+      this.CmdNumber8.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber8.TabIndex = 7;
+      this.CmdNumber8.Text = "8";
+      this.CmdNumber8.UseVisualStyleBackColor = true;
+      this.CmdNumber8.Click += new System.EventHandler(this.CmdNumber8_Click);
+      // 
+      // CmdNumber9
+      // 
+      this.CmdNumber9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber9.Location = new System.Drawing.Point(162, 196);
+      this.CmdNumber9.Name = "CmdNumber9";
+      this.CmdNumber9.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber9.TabIndex = 8;
+      this.CmdNumber9.Text = "9";
+      this.CmdNumber9.UseVisualStyleBackColor = true;
+      this.CmdNumber9.Click += new System.EventHandler(this.CmdNumber9_Click);
+      // 
+      // CmdNumber0
+      // 
+      this.CmdNumber0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdNumber0.Location = new System.Drawing.Point(92, 430);
+      this.CmdNumber0.Name = "CmdNumber0";
+      this.CmdNumber0.Size = new System.Drawing.Size(40, 40);
+      this.CmdNumber0.TabIndex = 9;
+      this.CmdNumber0.Text = "0";
+      this.CmdNumber0.UseVisualStyleBackColor = true;
+      this.CmdNumber0.Click += new System.EventHandler(this.CmdNumber0_Click);
+      // 
+      // CmdOperationResult
+      // 
+      this.CmdOperationResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdOperationResult.Location = new System.Drawing.Point(241, 430);
+      this.CmdOperationResult.Name = "CmdOperationResult";
+      this.CmdOperationResult.Size = new System.Drawing.Size(40, 40);
+      this.CmdOperationResult.TabIndex = 10;
+      this.CmdOperationResult.Text = "=";
+      this.CmdOperationResult.UseVisualStyleBackColor = true;
+      this.CmdOperationResult.Click += new System.EventHandler(this.CmdOperationResult_Click);
+      // 
+      // CmdOperationPlus
+      // 
+      this.CmdOperationPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdOperationPlus.Location = new System.Drawing.Point(241, 351);
+      this.CmdOperationPlus.Name = "CmdOperationPlus";
+      this.CmdOperationPlus.Size = new System.Drawing.Size(40, 40);
+      this.CmdOperationPlus.TabIndex = 11;
+      this.CmdOperationPlus.Text = "+";
+      this.CmdOperationPlus.UseVisualStyleBackColor = true;
+      this.CmdOperationPlus.Click += new System.EventHandler(this.CmdOperationPlus_Click);
+      // 
+      // CmdOperationMinus
+      // 
+      this.CmdOperationMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdOperationMinus.Location = new System.Drawing.Point(241, 274);
+      this.CmdOperationMinus.Name = "CmdOperationMinus";
+      this.CmdOperationMinus.Size = new System.Drawing.Size(40, 40);
+      this.CmdOperationMinus.TabIndex = 12;
+      this.CmdOperationMinus.Text = "-";
+      this.CmdOperationMinus.UseVisualStyleBackColor = true;
+      this.CmdOperationMinus.Click += new System.EventHandler(this.CmdOperationMinus_Click);
+      // 
+      // CmdOperationMultiply
+      // 
+      this.CmdOperationMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdOperationMultiply.Location = new System.Drawing.Point(241, 196);
+      this.CmdOperationMultiply.Name = "CmdOperationMultiply";
+      this.CmdOperationMultiply.Size = new System.Drawing.Size(40, 40);
+      this.CmdOperationMultiply.TabIndex = 13;
+      this.CmdOperationMultiply.Text = "×";
+      this.CmdOperationMultiply.UseVisualStyleBackColor = true;
+      this.CmdOperationMultiply.Click += new System.EventHandler(this.CmdOperationMultiply_Click);
+      // 
+      // CmdOperationComma
+      // 
+      this.CmdOperationComma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdOperationComma.Location = new System.Drawing.Point(162, 430);
+      this.CmdOperationComma.Name = "CmdOperationComma";
+      this.CmdOperationComma.Size = new System.Drawing.Size(40, 40);
+      this.CmdOperationComma.TabIndex = 14;
+      this.CmdOperationComma.Text = ",";
+      this.CmdOperationComma.UseVisualStyleBackColor = true;
+      this.CmdOperationComma.Click += new System.EventHandler(this.CmdOperationComma_Click);
+      // 
+      // CmdOperationRoot
+      // 
+      this.CmdOperationRoot.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdOperationRoot.Location = new System.Drawing.Point(22, 430);
+      this.CmdOperationRoot.Name = "CmdOperationRoot";
+      this.CmdOperationRoot.Size = new System.Drawing.Size(40, 40);
+      this.CmdOperationRoot.TabIndex = 15;
+      this.CmdOperationRoot.Text = "√";
+      this.CmdOperationRoot.UseVisualStyleBackColor = true;
+      this.CmdOperationRoot.Click += new System.EventHandler(this.CmdOperationRoot_Click);
+      // 
+      // LblResultShow
+      // 
+      this.LblResultShow.AutoSize = true;
+      this.LblResultShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.LblResultShow.ForeColor = System.Drawing.SystemColors.Desktop;
+      this.LblResultShow.Location = new System.Drawing.Point(33, 41);
+      this.LblResultShow.Name = "LblResultShow";
+      this.LblResultShow.Size = new System.Drawing.Size(29, 31);
+      this.LblResultShow.TabIndex = 16;
+      this.LblResultShow.Text = "0";
+      // 
+      // CmdReset
+      // 
+      this.CmdReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdReset.Location = new System.Drawing.Point(22, 119);
+      this.CmdReset.Name = "CmdReset";
+      this.CmdReset.Size = new System.Drawing.Size(40, 40);
+      this.CmdReset.TabIndex = 17;
+      this.CmdReset.Text = "C";
+      this.CmdReset.UseVisualStyleBackColor = true;
+      this.CmdReset.Click += new System.EventHandler(this.CmdReset_Click);
+      // 
+      // CmdOperationDivide
+      // 
+      this.CmdOperationDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.CmdOperationDivide.Location = new System.Drawing.Point(241, 119);
+      this.CmdOperationDivide.Name = "CmdOperationDivide";
+      this.CmdOperationDivide.Size = new System.Drawing.Size(40, 40);
+      this.CmdOperationDivide.TabIndex = 18;
+      this.CmdOperationDivide.Text = "÷";
+      this.CmdOperationDivide.UseVisualStyleBackColor = true;
+      this.CmdOperationDivide.Click += new System.EventHandler(this.CmdOperationDivide_Click);
+      // 
+      // LblCopyright
+      // 
+      this.LblCopyright.AutoSize = true;
+      this.LblCopyright.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.LblCopyright.Location = new System.Drawing.Point(118, 139);
+      this.LblCopyright.Name = "LblCopyright";
+      this.LblCopyright.Size = new System.Drawing.Size(59, 20);
+      this.LblCopyright.TabIndex = 19;
+      this.LblCopyright.Text = "© Jona";
+      // 
+      // Tachenrechner
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.Control;
+      this.ClientSize = new System.Drawing.Size(309, 488);
+      this.Controls.Add(this.LblCopyright);
+      this.Controls.Add(this.CmdOperationDivide);
+      this.Controls.Add(this.CmdReset);
+      this.Controls.Add(this.LblResultShow);
+      this.Controls.Add(this.CmdOperationRoot);
+      this.Controls.Add(this.CmdOperationComma);
+      this.Controls.Add(this.CmdOperationMultiply);
+      this.Controls.Add(this.CmdOperationMinus);
+      this.Controls.Add(this.CmdOperationPlus);
+      this.Controls.Add(this.CmdOperationResult);
+      this.Controls.Add(this.CmdNumber0);
+      this.Controls.Add(this.CmdNumber9);
+      this.Controls.Add(this.CmdNumber8);
+      this.Controls.Add(this.CmdNumber7);
+      this.Controls.Add(this.CmdNumber4);
+      this.Controls.Add(this.CmdNumber5);
+      this.Controls.Add(this.CmdNumber6);
+      this.Controls.Add(this.CmdNumber3);
+      this.Controls.Add(this.CmdNumber2);
+      this.Controls.Add(this.CmdNumber1);
+      this.ForeColor = System.Drawing.SystemColors.Desktop;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.KeyPreview = true;
+      this.Name = "Tachenrechner";
+      this.Text = "Tachenrechner";
+      this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Tachenrechner_KeyDown);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    private System.Windows.Forms.Button CmdNumber1;
+    private System.Windows.Forms.Button CmdNumber2;
+    private System.Windows.Forms.Button CmdNumber3;
+    private System.Windows.Forms.Button CmdNumber6;
+    private System.Windows.Forms.Button CmdNumber5;
+    private System.Windows.Forms.Button CmdNumber4;
+    private System.Windows.Forms.Button CmdNumber7;
+    private System.Windows.Forms.Button CmdNumber8;
+    private System.Windows.Forms.Button CmdNumber9;
+    private System.Windows.Forms.Button CmdNumber0;
+    private System.Windows.Forms.Button CmdOperationResult;
+    private System.Windows.Forms.Button CmdOperationPlus;
+    private System.Windows.Forms.Button CmdOperationMinus;
+    private System.Windows.Forms.Button CmdOperationMultiply;
+    private System.Windows.Forms.Button CmdOperationComma;
+    private System.Windows.Forms.Button CmdOperationRoot;
+    private System.Windows.Forms.Label LblResultShow;
+    private System.Windows.Forms.Button CmdReset;
+    private System.Windows.Forms.Button CmdOperationDivide;
+    private System.Windows.Forms.Label LblCopyright;
+  }
+}
+
---
+++ Calculator/FormCalculator.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+  public partial class Tachenrechner : Form
+  {
+
+    //Status ob man die Rechnung beendet wurde
+    private bool finish;
+
+    //Status ob ein Komma gesetzt wurde
+    private bool comma;
+    public Tachenrechner()
+    {
+      InitializeComponent();
+      finish = false;
+      comma = false;
+    }
+
+    #region "Button Click Events"
+    //Resetet das Label
+    private void CmdReset_Click(object sender, EventArgs e)
+    {
+      LblResultShow.Text = "0";
+      finish = false;
+    }
+
+    //Eingabe der Zahlen, Rechenoperation
+    private void CmdNumber1_Click(object sender, EventArgs e)
+    {
+      enterNumber(1);
+    }
+
+    private void CmdNumber2_Click(object sender, EventArgs e)
+    {
+      enterNumber(2);
+    }
+
+    private void CmdNumber3_Click(object sender, EventArgs e)
+    {
+      enterNumber(3);
+    }
+
+    private void CmdNumber4_Click(object sender, EventArgs e)
+    {
+      enterNumber(4);
+    }
+
+    private void CmdNumber5_Click(object sender, EventArgs e)
+    {
+      enterNumber(5);
+    }
+
+    private void CmdNumber6_Click(object sender, EventArgs e)
+    {
+      enterNumber(6);
+    }
+
+    private void CmdNumber7_Click(object sender, EventArgs e)
+    {
+      enterNumber(7);
+    }
+
+    private void CmdNumber8_Click(object sender, EventArgs e)
+    {
+      enterNumber(8);
+    }
+
+    private void CmdNumber9_Click(object sender, EventArgs e)
+    {
+      enterNumber(9);
+    }
+
+    private void CmdNumber0_Click(object sender, EventArgs e)
+    {
+      if (!(firstNumber()))
+      {
+        if (finish)
+        {
+          LblResultShow.Text = "0";
+          finish = false;
+        }
+        else
+        {
+          LblResultShow.Text = LblResultShow.Text + "0";
+        }
+      }
+    }
+    
+    private void CmdOperationComma_Click(object sender, EventArgs e)
+    {
+      enterOperation(",");
+    }
+
+    private void CmdOperationPlus_Click(object sender, EventArgs e)
+    {
+      enterOperation("+");
+    }
+
+    private void CmdOperationMinus_Click(object sender, EventArgs e)
+    {
+      enterOperation("-");
+    }
+
+    private void CmdOperationMultiply_Click(object sender, EventArgs e)
+    {
+      enterOperation("*");
+    }
+
+    private void CmdOperationDivide_Click(object sender, EventArgs e)
+    {
+      enterOperation("/");
+    }
+
+    private void CmdOperationRoot_Click(object sender, EventArgs e)
+    {
+      enterOperation("?");
+    }
+
+    //Befehl zum Ausgabe des Ergebnisses
+    private void CmdOperationResult_Click(object sender, EventArgs e)
+    {
+      if (!(finish))
+      {
+        string all;
+        all = LblResultShow.Text;
+
+
+        if (all.Contains("+"))
+        {
+          printResult("+");
+        }
+        else if (all.Contains("-"))
+        {
+          printResult("-");
+        }
+        else if (all.Contains("/"))
+        {
+          printResult("/");
+        }
+        else if (all.Contains("*"))
+        {
+          printResult("*");
+        }
+        else if (all.Contains("?"))
+        {
+          printResult("?");
+        }
+      }
+    }
+    #endregion
+
+    #region "Basis Logik"
+   
+    //Eingabe einer Zahl
+    private void enterNumber(double x)
+    {
+      if (firstNumber())
+      {
+        LblResultShow.Text = x.ToString();
+      }
+      else
+      {
+        if (finish)
+        {
+          LblResultShow.Text = x.ToString();
+          finish = false;
+        }
+        else
+        {
+          LblResultShow.Text = LblResultShow.Text + x.ToString();
+        }
+      }
+    }
+
+    //Eingabe einer Recheoperation
+    private void enterOperation(string operation)
+    {
+      //Überprüfung ob noch keine Rechenoperation schon angeben wurde
+      if (!(LblResultShow.Text.Contains("+") || LblResultShow.Text.Contains("-") || LblResultShow.Text.Contains("/") || LblResultShow.Text.Contains("*") || LblResultShow.Text.Contains("?")))
+      {
+        if (!(firstNumber()))
+        {
+          if (operation == ",")
+          {
+            if (!(comma))
+            {
+              LblResultShow.Text = LblResultShow.Text + operation;
+              comma = true;
+            }
+          }
+          else
+          {
+            LblResultShow.Text = LblResultShow.Text + " " + operation + " ";
+          }
+        }
+        else
+        {
+          if (operation == "?")
+          {
+            LblResultShow.Text = operation;
+          }
+        }
+      }
+      //Überprüfung ob eine Rechenoperation schon angeben wurde
+      else if (LblResultShow.Text.Contains("+") || LblResultShow.Text.Contains("-") || LblResultShow.Text.Contains("/") || LblResultShow.Text.Contains("*") || LblResultShow.Text.Contains("?"))
+      {
+        if (operation == ",")
+        {
+          if (comma)
+          {
+            LblResultShow.Text = LblResultShow.Text + operation;
+            comma = false;
+          }
+        }
+      }
+    }
+
+    //Augabe eines Ergebnisses
+    private void printResult(string operation)
+    {
+      double firstNumber;
+      double secondNumber;
+      string all;
+      char op;
+      op = operation.ToCharArray()[0];
+      all = LblResultShow.Text;
+
+      //Ausgabe und zusammen Rechnung des Ergebnisses vom Wurzel rechnen
+      if (operation == "?")
+      {
+        long number;
+        number = long.Parse(all.Replace("?", " "));
+
+        LblResultShow.Text = $"?{number} = {Math.Round(Math.Sqrt(number), 5)}";
+        finish = true;
+        return;
+      }
+
+      string[] st = all.Split(op);
+      firstNumber = double.Parse(st[0]);
+      secondNumber = double.Parse(st[1].TrimEnd('='));
+      //Ausgabe und zusammen Rechnung des Ergebnisses der vier Grundrechenarten
+      if (operation == "+")
+      {
+        LblResultShow.Text = $"{firstNumber} + {secondNumber} = {(firstNumber + secondNumber)}";
+      }
+      else if (operation == "-")
+      {
+        LblResultShow.Text = $"{firstNumber} - {secondNumber} = {(firstNumber - secondNumber)}";
+      }
+      else if (operation == "/")
+      {
+        LblResultShow.Text = $"{firstNumber} / {secondNumber} = {(firstNumber / secondNumber)}";
+      }
+      else if (operation == "*")
+      {
+        LblResultShow.Text = $"{firstNumber} * {secondNumber} = {(firstNumber * secondNumber)}";
+      }
+      finish = true;
+    }
+    #endregion
+
+    #region "Key Press Event
+    //Eingabe der Zahlen und Rechenoperationen durch die Tastatur.
+    private void Tachenrechner_KeyDown(object sender, KeyEventArgs e)
+    {
+      //Zahlen
+      if (e.KeyCode == Keys.D1 || e.KeyCode == Keys.NumPad1)
+      {
+        enterNumber(1);
+      }
+      else if (e.KeyCode == Keys.D2 || e.KeyCode == Keys.NumPad2)
+      {
+        enterNumber(2);
+      }
+      else if (e.KeyCode == Keys.D3 || e.KeyCode == Keys.NumPad3)
+      {
+        enterNumber(3);
+      }
+      else if (e.KeyCode == Keys.D4 || e.KeyCode == Keys.NumPad4)
+      {
+        enterNumber(4);
+      }
+      else if (e.KeyCode == Keys.D5 || e.KeyCode == Keys.NumPad5)
+      {
+        enterNumber(5);
+      }
+      else if (e.KeyCode == Keys.D6 || e.KeyCode == Keys.NumPad6)
+      {
+        enterNumber(6);
+      }
+      else if (e.KeyCode == Keys.D7 || e.KeyCode == Keys.NumPad7)
+      {
+        enterNumber(7);
+      }
+      else if (e.KeyCode == Keys.D8 || e.KeyCode == Keys.NumPad8)
+      {
+        enterNumber(8);
+      }
+      else if (e.KeyCode == Keys.D9 || e.KeyCode == Keys.NumPad9)
+      {
+        enterNumber(9);
+      }
+      else if (e.KeyCode == Keys.D0 || e.KeyCode == Keys.NumPad0)
+      {
+        enterNumber(0);
+      }
+      else if (e.KeyCode == Keys.Oemplus || e.KeyCode == Keys.Add)
+      {
+        enterOperation("+");
+      }
+      else if (e.KeyCode == Keys.OemMinus || e.KeyCode == Keys.Subtract)
+      {
+        enterOperation("-");
+      }
+      else if (e.KeyCode == Keys.Multiply)
+      {
+        enterOperation("*");
+      }
+      else if (e.KeyCode == Keys.Divide)
+      {
+        enterOperation("/");
+      }
+      else if (e.KeyCode == Keys.Oemcomma || e.KeyCode == Keys.Decimal)
+      {
+        enterOperation(",");
+      }
+      else if (e.KeyCode == Keys.Enter)
+      {
+        string all;
+        all = LblResultShow.Text;
+
+
+        if (all.Contains("+"))
+        {
+          printResult("+");
+        }
+        else if (all.Contains("-"))
+        {
+          printResult("-");
+        }
+        else if (all.Contains("/"))
+        {
+          printResult("/");
+        }
+        else if (all.Contains("*"))
+        {
+          printResult("*");
+        }
+        else if (all.Contains("?"))
+        {
+          printResult("?");
+        }
+      }
+    }
+    #endregion
+
+    #region "Checks"
+    //Überprüfung ob es die erste Zahl ist.
+    private bool firstNumber()
+    {
+      bool status;
+      if (LblResultShow.Text == "0")
+      {
+        status = true;
+      }
+      else
+      {
+        status = false;
+      }
+      return status;
+    }
+    #endregion
+  }
+}
